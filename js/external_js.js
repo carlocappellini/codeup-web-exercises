4f3c63b71f479b892f8f6afa7e014501d3288778
Bolimbo <carlo.cappellini@yahoo.com>
2017-08-15 09:16:40 -0500

    javascript

---
+++ js/external_js.js
@@ -0,0 +1,15 @@
+
+
+console.log('Hello from external Javascript')
+alert('Welcome to my website')
+
+
+var color = prompt("what is your favorite color?")
+
+if (color === "green") {
+    alert(color + " is my favorite color")
+} else {
+    alert("is not my favorite color")
+}
+
+
