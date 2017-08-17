8a0d059bb04bbfeb5f1f6491a356339db25b24b1
Bolimbo <carlo.cappellini@yahoo.com>
2017-08-17 12:03:11 -0500

    for loops,while loops, do while loops updated with my code and instructors code

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,18 +1,55 @@
-var question = prompt("Write a number 1 - 7")
+"use strict";
+
+// var question = prompt("Write a number 1 - 7")
+//
+//
+// for (var i = 1; i <= 10; i++) {
+//     console.log(question + " X " + i + " = " + i * question )
+// }
+//
 
 
-for (var i = 1; i <= 10; i++) {
-    console.log(question + " X " + i + " = " + i * question )
-}
 
 
-for (var i = 1; i <=10; i++) {
-    var numbers = Math.floor(Math.random() * 20) + 180;
+
+
+for (var i = 1; i < 10; i++) {
+    var numbers = Math.floor(Math.random() * 180) + 20;
     if (numbers % 2 === 0) {
         console.log(numbers + " is even");
     } else {
-        console.log(numbers + " is odd")
+        console.log(numbers + " is odd");
     }
 }
 
 
+// Write a JavaScript program to construct the following pattern, using a nested for loop. Promt the user the number of the final lenght of the stars.
+//
+//     Example:
+//
+// User input: 5
+//
+// Result:
+//
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// var stars = 5;
+//
+// var question = prompt("Number of final stars \n* * * * *")
+//
+// console.log("User input = " + stars)
+// console.log("result : ")
+//
+// for (var i = 1; i <= question; ++i) {
+//     var str = '';
+//     for (var j = 1; j <= i; ++j){
+//         str += "* ";
+//     }
+//     console.log(str);
+// }
+//
+//
--- js/while.js
+++ js/while.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 var i = 1;
 
@@ -8,22 +8,72 @@ var i = 1;
 
 }
 
+
+//---------------
+// thi is also gives the same result
+
+var limit = 65536;
+
+  var powersOfTwo = 2; /*accumulator*/
+ while (powersOfTwo <= limit) {
+     console.log(powersOfTwo)
+     //powersoftwo = powersoftwo *= 2;
+     powersOfTwo *= 2;
+
+ }
+
+
+
+
+// ---------------------
+
+
+
+
+
+// This is how you get a random number between 50 and 100
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// // This is how you get a random number between 1 and 5
+//
+// var conesSold = 5;
+// var notEnoughCones;
+//
+// do {
+//     var cones = Math.floor(Math.random() * 5) + 1;
+//     console.log(cones + " cones please")
+//
+//     if (allCones >= cones){
+//         allCones = allCones - cones;
+//         console.log("I have " + allCones + " left");
+//     } else {
+//         console.log("Dont have enough cones, I have " + allCones + " left")
+//     }
+//     notEnoughCones = (allCones > 0);
+// } while (notEnoughCones);
+//----------------
+
+//this also gives same result
+
+
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 // This is how you get a random number between 1 and 5
 
-var conesSold = 5;
-var notEnoughCones;
+
 
 do {
     var cones = Math.floor(Math.random() * 5) + 1;
-    console.log(cones + " cones please")
+    if(cones > allCones) {
+        console.log("cannot sell you " + cones + " i only have " + allCones);
+        continue;
 
-    if (allCones >= cones){
-        allCones = allCones - cones;
-        console.log("I have " + allCones + " left");
-    } else {
-        console.log("Dont have enough cones, I have " + allCones + " left")
     }
-    notEnoughCones = (allCones > 0);
-} while (notEnoughCones);
+    allCones -= cones;
+    console.log(cones + " cones sold")
+
+} while (allCones > 0)
+console.log("yay I sold them all")
+
+
+
+//if u know doa for loop, if u dont kno do a while loop or do while, if once do while
