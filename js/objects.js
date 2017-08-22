eee84f73e93e930ae7f1fea51459ee49c6d0915f
Bolimbo <carlo.cappellini@yahoo.com>
2017-08-22 11:45:44 -0500

    finished objects exercises

--- js/objects.js
+++ js/objects.js
@@ -1,46 +1,49 @@
+
 (function(){
     "use strict";
 
-    /**
-     * TODO:
-     * Create person object, store it in a variable named person
-     */
+    var person = {};  // literal object representation
 
-    /**
-     * TODO:
-     * Create firstName and lastName properties in your person object, and
-     * assign your name to them
-     */
+    person.firstName = "Luis";
+    person.lastName = "Montealegre";
 
-    /**
-     * TODO:
-     * Add a sayHello method to the person object that returns a greeting using
-     * the firstName and lastName properties.
-     * console.log the returned message to check your work
-     *
-     * Example
-     * person.sayHello() // returns "Hello from Rick Sanchez!"
-     */
+    person.sayHello = function() {
+        return "Hello from " + this.firstName + " " + this.lastName + "!";
+    }; // literal function representation
 
-    /** TODO: Remember this problem from before?
-     *
-     * HEB has an offer for the shoppers that buy products amounting to more
-     * than $200. Write a JS program, using conditionals, that logs to the
-     * browser, how much does Ryan, Cameron and George need to pay. We know that
-     * Cameron bought $180, Ryan $250 and George $320. Your program will have to
-     * display a line with the name of the person, the amount before the
-     * discount, the discount, if any, and the amount after the discount.
-     *
-     * Uncomment the lines below to create an array of objects where each object
-     * represents one shopper. Use a foreach loop to iterate through the array,
-     * and console.log the relevant messages for each person
-     */
+    console.log(person.sayHello());
 
-    // var shoppers = [
-    //     {name: 'Cameron', amount: 180},
-    //     {name: 'Ryan', amount: 250},
-    //     {name: 'George', amount: 320}
-    // ];
+    var shoppers = [
+        {name: 'Cameron', amount: 180},
+        {name: 'Ryan', amount: 250},
+        {name: 'George', amount: 320}
+    ];
+
+    // 3. loop
+    // for loop
+    // foreach function
+    for (var i = 0; i < shoppers.length; i++) {
+        var shopper = shoppers[i]; // current shopper
+        console.log(buildReceipt(shopper));
+    }
+
+    shoppers.forEach(function (shopper) {
+        console.log(buildReceipt(shopper));
+    });
 
 
+    function buildReceipt(shopper) {
+        var discount = 0;
+        var totalAfterDiscount = shopper.amount;
+        // 2. Conditionals
+        if (shopper.amount > 200) {
+            discount = .1;
+            totalAfterDiscount = shopper.amount - shopper.amount * discount;
+        }
+        return shopper.name
+            + " has to pay $" + shopper.amount
+            + ", it has a discount of $" + discount
+            + ", the total to pay after the discount is $" + totalAfterDiscount
+            ;
+    }
 })();
