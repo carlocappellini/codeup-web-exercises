8d7d322639e87451480fc1ee21dbbc6f3b5d4ddd
Bolimbo <carlo.cappellini@yahoo.com>
2017-09-01 13:09:54 -0500

    study samples

--- js/study-examples.js
+++ js/study-examples.js
@@ -6,26 +6,31 @@
 // isNegative(-5) // true
 // isNegative(0) // false
 // isNegative(6) // false
+
 // Write a function named average that accepts an array of numbers and returns the average of those numbers.
 //
 // average([1, 2, 3]) // 2
 // average([4, 6, 8, 10, 12]) // 8
 // average([1, 2]) // 1.5
+
 // Write a function named countOdds that accepts an array of numbers and returns the number of odd numbers in the array.
 //
 // countOdds([1, 2, 3]) // 2
 // countOdds([4, 6, 8, 10]) // 0
 // countOdds([1, 2, 1, 1]) // 3
+
 // Write a function named convertNameToObject that accepts a string that contains a first name and last name separated by a space character, and returns an object with properties firstName and lastName.
 //
 // convertNameToObject('Harry Potter') // {firstName: 'Harry', lastName: 'Potter'}
 // convertNameToObject('Ron Weasley') // {firstName: 'Ron', lastName: 'Weasley'}
+
 // Write a function named fiveTo that accepts a number and returns an array of the integers from 5 up to and including the passed number.
 //
 // fiveTo(5)   // [5]
 // fiveTo(10)  // [5, 6, 7, 8, 9, 10]
 // fiveTo(-2)  // [5, 4, 3, 2, 1, 0, -1, -2]
 // fiveTo(6)   // [5, 6]
+
 // Write a function named upperCaseLastNames that accepts an array of objects where each object has properties firstName and lastName and returns the same array of objects with each last name capitalized
 //
 // var names = [
@@ -170,4 +175,21 @@
 //         $(this).css("background-color", 'red')
 //
 //     }, hoverOut)
-// })();
+// })();hoverOut
+
+
+// example 3 turn prpomt to upprtcase
+
+// function toUpperCase (message) {
+//     message = prompmessage.toUpperCase
+//     return  prompt(message.toUpperCase())
+// }
+
+//end exampple 3
+
+
+//koonami code exercise
+
+
+//end koonami
+
